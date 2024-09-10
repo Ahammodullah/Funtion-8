fe9ce705e2bf300f82378b190fd859a83ed879a1
Ahammod <ahammodullah12@gmail.com>
2024-09-10 23:57:30 +0600

    first commit

---
+++ Function.js
@@ -0,0 +1,69 @@
+//creating  function
+
+function ahammod(){
+
+var num = 7;
+var result=num*num;
+document.write("Result= " +result +"<br>")
+
+var num = 8;
+var result=num*num;
+document.write("Result= " +result +"<br>")
+
+var num = 9;
+var result=num*num;
+document.write("Result= " +result +"<br>")
+
+var num = 10;
+var result=num*num;
+document.write("Result= " +result +"<br>")
+
+var num = 11;
+var result=num*num;
+document.write("Result= " +result +"<br>")
+}
+//oporer calling  function
+ahammod();
+document.write("<br>")
+ahammod();
+document.write("<br>")
+
+
+
+//add function
+function addition(x,y){
+    var result= x+y;
+    document.write("sum=" +result+ "<br>");
+}
+addition(20,30)
+document.write("<br>")
+
+
+//sub function
+function subtraction(x,y){
+    var result= x-y;
+    document.write("sub=" +result+ "<br>");
+}
+subtraction(50,30)
+document.write("<br>")
+
+ 
+//count function
+function count(x,y){
+    var result= x*y;
+    document.write("count=" +result+ "<br>");
+}
+count(5,9)
+document.write("<br>")
+
+
+//quotient function
+function quotient(x,y){
+    var result= x/y;
+    document.write("quotient=" +result+ "<br>");
+}
+quotient(200,5)
+
+
+
+
